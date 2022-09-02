fe35f86e8d3edeb7413a285c721427d8c91657eb
Thomas Thorin <thomas.thorin@gmail.com>
2022-09-02 09:55:25 +0200

    ?

--- AspReact/ClientApp/src/App.jsx
+++ AspReact/ClientApp/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [weather,setWeather] = useState("")
 
   return (
     <div className="App">
@@ -19,7 +20,9 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+              </button>
+              <button onClick={async () => setWeather(await fetch("/api/weatherforecast").then(res => res.json).then(json=>console.log(json)))}>Fetch</button>
+              {console.log(weather) }
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
